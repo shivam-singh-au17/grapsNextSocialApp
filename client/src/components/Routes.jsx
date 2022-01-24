08e5544f80039a4c0c76ca65892c9f78f1d117f8
Shivam Singh <shivamsingh4458@gmail.com>
2022-01-24 22:38:48 +0530

    adding router file and css file

---
+++ client/src/components/Routes.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Login from "./Login/Login";
+import Registration from "./Registration/Registration";
+import "./style.css";
+
+const Routes = () => {
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Registration />
+        </Route>
+        <Route>
+          <div className="showError">
+            <div className="errorSection">
+              <div className="leftErrorImg">
+                <img
+                  src="https://github.com/shivam-singh-au17/resumeBuilder/blob/master/src/components/img/error.png?raw=true"
+                  alt=""
+                />
+              </div>
+              <div className="rightRror">
+                <button className="btn btn-secondary fs-5 mb-3" type="button">
+                  <Link className="nav-link text-light" to="/">
+                    Back to home
+                  </Link>
+                </button>
+                <p className="pageNotFound">PAGE NOT FOUND</p>
+                <p className="notFound">404</p>
+                <p className="notFoundTxt">Out of nothing, something.</p>
+              </div>
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </>
+  );
+};
+
+export default Routes;
