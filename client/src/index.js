5d37789b305b3c94fd5a3cd0062a424f76c01751
Shivam Singh <shivamsingh4458@gmail.com>
2022-01-25 18:08:48 +0530

    update client

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Theme1 from './components/Contexts/ThemeContexts1';
+import Theme2 from './components/Contexts/ThemeContexts2';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Theme1>
+      <Theme2>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Theme2>
+    </Theme1>
   </React.StrictMode>,
   document.getElementById('root')
 );
