101b6191cd8380b9b7b9bfec704228d3eb94de28
Shivam Singh <shivamsingh4458@gmail.com>
2022-01-25 18:06:59 +0530

    update logiin

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import SlidShow from "../SlidShow/SlidShow";
 import { Link } from "react-router-dom";
 import "../style.css";
+import axios from "axios";
+import { ThemeContexts1 } from "../Contexts/ThemeContexts1";
+import { ThemeContexts2 } from "../Contexts/ThemeContexts2";
 
 const Login = () => {
+  const [status, setStatus] = useState(false);
+  const { handleChangeTheme1 } = useContext(ThemeContexts1);
+  const { setTheme2 } = useContext(ThemeContexts2);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    const jsonData = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post("https://server-by-shivam-singh.herokuapp.com/login", jsonData)
+      .then((data) => {
+        setTheme2(data.data);
+        if (data.data.token) {
+          alert("Successfully Login ...");
+          handleChangeTheme1();
+        }
+      })
+      .catch(() => {
+        setStatus(true);
+      });
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="row loginMaxWidth">
       <div className=".col-xl-12 col-6 mt-4 SlidShowBox">
@@ -25,6 +56,8 @@ const Login = () => {
               id="Email"
               aria-describedby="emailHelp"
               placeholder="Enter Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
@@ -39,12 +72,25 @@ const Login = () => {
               className="form-control"
               id="Password"
               placeholder="Enter Your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit" className="btn mt-3 btn-success fw-bold">
+          <button
+            type="submit"
+            className="btn mt-3 btn-success fw-bold"
+            onClick={handleLogin}
+          >
             LOG IN
           </button>
-          <div className="bg-secondary fs-5 p-3 my-5 accountBox">
+          {status ? (
+            <div id="emailHelp" className="form-text text-danger fw-bold mt-2">
+              Please provide correct information ...
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="bg-secondary fs-5 p-3 my-4 accountBox">
             <span className="text-light ms-5"> Don't have an account yet?</span>
             <Link className="nav-link text-light" to="/signup">
               <button className="btn btn-info fw-bold">SIGN UP</button>
