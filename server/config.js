16d079936ffac6472126b438f66218b9a8a0a81b
Shivam Singh <shivamsingh4458@gmail.com>
2022-01-25 09:42:52 +0530

    update

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    JWT_SECRET_KEY: "authLayerauthLayerauthLayerauthLayer",
-    GOOGLE_CLIENT_ID: "319991899435-dahdsfavggh5mbtu40qdpt3o3bgmgnsm.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
-    GOOGLE_CALLBACK_URL: "http://localhost:3333/auth/google/callback",
-};
