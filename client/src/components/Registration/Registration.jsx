61df1f32780267b87b9451d081062828a9b068b9
Shivam Singh <shivamsingh4458@gmail.com>
2022-01-25 09:43:54 +0530

    update in src

--- client/src/components/Registration/Registration.jsx
+++ client/src/components/Registration/Registration.jsx
@@ -1,9 +1,30 @@
-import React from "react";
-import google from "../img/google.png";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../style.css";
+import axios from "axios";
 
 const Registration = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = () => {
+    const jsonData = {
+      name: name,
+      email: email,
+      mobile: phone,
+      password: password,
+    };
+
+    axios.post("http://localhost:4000/register", jsonData);
+    alert("Successfully Submited ...");
+    setName("");
+    setEmail("");
+    setPhone("");
+    setPassword("");
+  };
+
   return (
     <div className="RegistrationBox mt-4">
       <div className="mt-2">
@@ -17,69 +38,71 @@ const Registration = () => {
             <span className="display-6">raps Next Social</span>
           </div>
         </div>
-        <div className="mb-2">
-          <label htmlFor="exampleInputPassword1" className="form-label fs-5">
+        <div className="mb-3">
+          <label htmlFor="Name" className="form-label fs-5">
             Name
           </label>
           <input
-            type="text"
+            type="name"
             className="form-control"
-            id="exampleInputPassword1"
+            id="Name"
             placeholder="Enter Your Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <div className="mb-2">
-          <label htmlFor="exampleInputEmail1" className="form-label fs-5">
+        <div className="mb-3">
+          <label htmlFor="Email" className="form-label fs-5">
             Email
           </label>
           <input
             type="email"
             className="form-control"
-            id="exampleInputEmail1"
+            id="Email"
             aria-describedby="emailHelp"
             placeholder="Enter Your Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
           </div>
         </div>
-        <div className="mb-2">
-          <label htmlFor="exampleInputPassword1" className="form-label fs-5">
+        <div className="mb-3">
+          <label htmlFor="Mobile" className="form-label fs-5">
             Mobile
           </label>
           <input
             type="text"
             className="form-control"
-            id="exampleInputPassword1"
+            id="Mobile"
             placeholder="Enter Your Mobile"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
           />
         </div>
-        <div className="mb-2">
-          <label htmlFor="exampleInputPassword1" className="form-label fs-5">
+        <div className="mb-3">
+          <label htmlFor="Password" className="form-label fs-5">
             Password
           </label>
           <input
             type="password"
             className="form-control"
-            id="exampleInputPassword1"
+            id="Password"
             placeholder="Enter Your Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button type="submit" className="btn btn-success fs-5">
+        <button
+          type="submit"
+          className="btn btn-success fs-5"
+          onClick={handleSignup}
+        >
           SIGN UP
         </button>
-        <div class="d-grid gap-2 my-3">
-          <button
-            class="btn btn-primary text-center RegistrationbuttonGoogle"
-            type="button"
-          >
-            <div>
-              <img className="ms-5 me-4" src={google} alt="google" />
-              <span className="fs-5">Log in with Google</span>
-            </div>
-          </button>
-        </div>
-        <div className="bg-secondary fs-5 mb-4">
+
+        <div className="bg-secondary fs-5 my-4">
           <div className="RegistrationaccountBox">
             <span className="text-light">Have an account?</span>
             <Link className="nav-link text-light" to="/">
